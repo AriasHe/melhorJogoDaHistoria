7d1f6bc4187974814a3e320c56ed297aac3e9e9e
Mariana H. Arias <m239844@dac.unicamp.br>
2026-07-13 01:44:44 -0300

    Inserindo a classe Aba.java
    Nela é possível inicializar uma janela para começar a realizar a
    construção do jogo

---
+++ projeto_finaal/src/main/java/com/melhorJogoDaHistoria/Aba.java
@@ -0,0 +1,36 @@
+package com.melhorJogoDaHistoria;
+
+import javax.swing.JFrame;
+
+
+public class Aba {
+    protected final int tileSize = 48;
+    protected final int maxScreenCol = 16;
+    protected final int maxScreenLin = 12;
+    protected final int linScreen = tileSize*maxScreenLin;
+    protected final int colScreen = tileSize*maxScreenCol;
+    protected JFrame frame;
+
+  public Aba() {
+
+  }
+  public void painel(){
+    Mecanica mecanica = new Mecanica();
+    mecanica.inicializacao();
+    frame.add(mecanica);
+    frame.pack();
+    mecanica.inicioThread();
+  }
+
+  public void cirar() {
+    
+    frame = new JFrame("titulo");
+    frame.setVisible(true);
+    frame.setSize(colScreen, linScreen);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+  }
+
+}
