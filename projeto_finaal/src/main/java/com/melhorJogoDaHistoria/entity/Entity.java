7dff9e6c3118b356dd190f417498065f4af8e80c
Mariana H. Arias <m239844@dac.unicamp.br>
2026-07-16 00:07:44 -0300

    Remover atributos não usados do Entity

--- projeto_finaal/src/main/java/com/melhorJogoDaHistoria/entity/Entity.java
+++ projeto_finaal/src/main/java/com/melhorJogoDaHistoria/entity/Entity.java
@@ -3,8 +3,6 @@ import java.awt.image.BufferedImage;
 
 
 public abstract class Entity {
-  //protected AcertoContas acerto = new AcertoContas();
-  protected BufferedImage boneco;
   protected int forca,vida,defesa,ataque,x,y;
   protected String nome,texto;
   public abstract BufferedImage PlayerImage();
